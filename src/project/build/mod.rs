f5e6225a3f27bc08cbcedfcae108bdab425343c8
Aleksey Kalsin <aleksey.kalsin@devim.team>
2026-09-07 19:41:32 +0300

    feat(build): Добавлена сборка проектов workspace

--- src/project/build/mod.rs
+++ src/project/build/mod.rs
@@ -5,8 +5,8 @@ mod plan;
 mod settings;
 mod tool;
 
-pub use execute::{BuildError, BuildResult, BuildStage, execute, execute_streaming};
-pub use plan::{ArtifactType, BuildPlan, PlanError};
+pub use execute::{BuildError, BuildResult, BuildStage, execute, execute_streaming, preflight};
+pub use plan::{ArtifactType, BuildPlan, PlanError, validate_unique_outputs};
 pub use settings::{
     BuildSettings, BuildSettingsError, InvalidArtifactsDirectoryReason, PlatformVersion,
 };
--- src/project/selection.rs
+++ src/project/selection.rs
@@ -6,6 +6,7 @@ use super::{Project, ProjectName, ProjectNameError, discovery::DiscoveryContext}
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
 pub enum SelectionIntent {
     ReadOnly,
+    MultipleMutation,
     SingleMutation,
 }
 
